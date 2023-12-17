4264bc94ebb22553f0cccdbfc9e977580dac2ce9
AbdenourTadjer33 <tad.abdenour33@gmail.com>
2023-12-17 02:25:22 +0100

    dashboard view

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,29 +1,113 @@
 <x-admin-layout>
 
+    <div class="pt-7 pb-5 bg-gradient-to-t from-fuchsia-100 via-purple-100 to-fuchsia-100">
+        <div class="flex flex-wrap mb-7">
 
-    <div class="m-3 flex flex-wrap">
-
-        <div class="w-full max-w-full px-3 mb-6 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/4">
-            <div
-                class="relative flex flex-col min-w-0 break-words bg-white shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
-                <div class="flex-auto p-4">
-                    <div class="flex flex-row -mx-3">
-                        <div class="flex-none w-2/3 max-w-full px-3">
-                            <div >
-                                <p
-                                    class="mb-0 font-sans font-semibold leading-normal uppercase dark:opacity-60 text-sm">
-                                    all booking</p>
-                                <h5 class="mb-2 font-bold">$53,000</h5>
-                                <p class="mb-0 dark:opacity-60">
-                                    <span class="font-bold leading-normal text-sm text-emerald-500">+55%</span>
-                                    since yesterday
-                                </p>
+            <div class="w-full max-w-full px-3 mb-6 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/4">
+                <div
+                    class="relative flex flex-col min-w-0 break-words bg-white shadow-xl bg-slate-850 shadow-dark-xl rounded-2xl bg-clip-border">
+                    <div class="flex-auto p-4">
+                        <div class="flex flex-row -mx-3">
+                            <div class="flex-none w-2/3 max-w-full px-3">
+                                <div>
+                                    <p class="mb-0 font-sans font-semibold leading-normal uppercase opacity-60 text-sm">
+                                        all booking</p>
+                                    <h5 class="mb-2 font-bold">$53,000</h5>
+                                    <p class="mb-0 opacity-60">
+                                        <span class="font-bold leading-normal text-sm text-emerald-500">+55%</span>
+                                        since yesterday
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="px-3 text-right basis-1/3">
+                                <div
+                                    class="inline-block w-12 h-12 text-center rounded-circle bg-gradient-to-tl from-blue-500 to-violet-500">
+                                    <i class="ni ni-money-coins text-lg relative top-3.5 text-white"></i>
+                                </div>
                             </div>
                         </div>
-                        <div class="px-3 text-right basis-1/3">
-                            <div
-                                class="inline-block w-12 h-12 text-center rounded-circle bg-gradient-to-tl from-blue-500 to-violet-500">
-                                <i class="ni ni-money-coins text-lg relative top-3.5 text-white"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div class="w-full max-w-full px-3 mb-6 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/4">
+                <div
+                    class="relative flex flex-col min-w-0 break-words bg-white shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
+                    <div class="flex-auto p-4">
+                        <div class="flex flex-row -mx-3">
+                            <div class="flex-none w-2/3 max-w-full px-3">
+                                <div>
+                                    <p
+                                        class="mb-0 font-sans font-semibold leading-normal uppercase dark:opacity-60 text-sm">
+                                        all booking</p>
+                                    <h5 class="mb-2 font-bold">$53,000</h5>
+                                    <p class="mb-0 dark:opacity-60">
+                                        <span class="font-bold leading-normal text-sm text-emerald-500">+55%</span>
+                                        since yesterday
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="px-3 text-right basis-1/3">
+                                <div
+                                    class="inline-block w-12 h-12 text-center rounded-circle bg-gradient-to-tl from-blue-500 to-violet-500">
+                                    <i class="ni ni-money-coins text-lg relative top-3.5 text-white"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="w-full max-w-full px-3 mb-6 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/4">
+                <div
+                    class="relative flex flex-col min-w-0 break-words bg-white shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
+                    <div class="flex-auto p-4">
+                        <div class="flex flex-row -mx-3">
+                            <div class="flex-none w-2/3 max-w-full px-3">
+                                <div>
+                                    <p
+                                        class="mb-0 font-sans font-semibold leading-normal uppercase dark:opacity-60 text-sm">
+                                        all booking</p>
+                                    <h5 class="mb-2 font-bold">$53,000</h5>
+                                    <p class="mb-0 dark:opacity-60">
+                                        <span class="font-bold leading-normal text-sm text-emerald-500">+55%</span>
+                                        since yesterday
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="px-3 text-right basis-1/3">
+                                <div
+                                    class="inline-block w-12 h-12 text-center rounded-circle bg-gradient-to-tl from-blue-500 to-violet-500">
+                                    <i class="ni ni-money-coins text-lg relative top-3.5 text-white"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="w-full max-w-full px-3 mb-6 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/4">
+                <div
+                    class="relative flex flex-col min-w-0 break-words bg-white shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
+                    <div class="flex-auto p-4">
+                        <div class="flex flex-row -mx-3">
+                            <div class="flex-none w-2/3 max-w-full px-3">
+                                <div>
+                                    <p
+                                        class="mb-0 font-sans font-semibold leading-normal uppercase dark:opacity-60 text-sm">
+                                        all booking</p>
+                                    <h5 class="mb-2 font-bold">$53,000</h5>
+                                    <p class="mb-0 dark:opacity-60">
+                                        <span class="font-bold leading-normal text-sm text-emerald-500">+55%</span>
+                                        since yesterday
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="px-3 text-right basis-1/3">
+                                <div
+                                    class="inline-block w-12 h-12 text-center rounded-circle bg-gradient-to-tl from-blue-500 to-violet-500">
+                                    <i class="ni ni-money-coins text-lg relative top-3.5 text-white"></i>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -31,6 +115,213 @@
             </div>
         </div>
 
+        <div class="flex items-center justify-center">
+            <div class="w-full mx-5">
+                <div class="-mx-2 md:flex">
 
+                    <div class="w-full md:w-1/3 px-2">
+                        <div class="rounded-lg shadow-sm mb-4">
+                            <div class="rounded-lg bg-white shadow-lg md:shadow-xl relative overflow-hidden">
+                                <div class="px-3 pt-8 pb-10 text-center relative z-10">
+                                    <h4 class="text-sm uppercase text-gray-500 leading-tight">Users</h4>
+                                    <h3 class="text-3xl text-gray-700 font-semibold leading-tight my-3">3,682</h3>
+                                    <p class="text-xs text-green-500 leading-tight">▲ 57.1%</p>
+                                </div>
+                                <div class="absolute bottom-0 inset-x-0">
+                                    <canvas id="chart1" height="70"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="w-full md:w-1/3 px-2">
+                        <div class="rounded-lg shadow-sm mb-4">
+                            <div class="rounded-lg bg-white shadow-lg md:shadow-xl relative overflow-hidden">
+                                <div class="px-3 pt-8 pb-10 text-center relative z-10">
+                                    <h4 class="text-sm uppercase text-gray-500 leading-tight">Users</h4>
+                                    <h3 class="text-3xl text-gray-700 font-semibold leading-tight my-3">3,682</h3>
+                                    <p class="text-xs text-green-500 leading-tight">▲ 57.1%</p>
+                                </div>
+                                <div class="absolute bottom-0 inset-x-0">
+                                    <canvas id="chart1" height="70"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="w-full md:w-1/3 px-2">
+                        <div class="rounded-lg shadow-sm mb-4">
+                            <div class="rounded-lg bg-white shadow-lg md:shadow-xl relative overflow-hidden">
+                                <div class="px-3 pt-8 pb-10 text-center relative z-10">
+                                    <h4 class="text-sm uppercase text-gray-500 leading-tight">Subscribers</h4>
+                                    <h3 class="text-3xl text-gray-700 font-semibold leading-tight my-3">11,427</h3>
+                                    <p class="text-xs text-red-500 leading-tight">▼ 42.8%</p>
+                                </div>
+                                <div class="absolute bottom-0 inset-x-0">
+                                    <canvas id="chart2" height="70"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="w-full md:w-1/3 px-2">
+                        <div class="rounded-lg shadow-sm mb-4">
+                            <div class="rounded-lg bg-white shadow-lg md:shadow-xl relative overflow-hidden">
+                                <div class="px-3 pt-8 pb-10 text-center relative z-10">
+                                    <h4 class="text-sm uppercase text-gray-500 leading-tight">Comments</h4>
+                                    <h3 class="text-3xl text-gray-700 font-semibold leading-tight my-3">8,028</h3>
+                                    <p class="text-xs text-green-500 leading-tight">▲ 8.2%</p>
+                                </div>
+                                <div class="absolute bottom-0 inset-x-0">
+                                    <canvas id="chart3" height="70"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+
+    <div class="mt-5 mx-auto max-w-full flex items-center justify-center">
+        <canvas style="max-width: 384px; max-height: 384px" id="bookingChart"></canvas>
+        <canvas style="max-width: 384px; max-height: 384px" id="bookingType"></canvas>
+    </div>
+
+
+    <script>
+        window.onload = () => {
+            // const chartOptions = {
+            //     maintainAspectRatio: false,
+            //     legend: {
+            //         display: false,
+            //     },
+            //     tooltips: {
+            //         enabled: false,
+            //     },
+            //     elements: {
+            //         point: {
+            //             radius: 0
+            //         },
+            //     },
+            //     scales: {
+            //         xAxes: [{
+            //             gridLines: false,
+            //             scaleLabel: false,
+            //             ticks: {
+            //                 display: false
+            //             }
+            //         }],
+            //         yAxes: [{
+            //             gridLines: false,
+            //             scaleLabel: false,
+            //             ticks: {
+            //                 display: false,
+            //                 suggestedMin: 0,
+            //                 suggestedMax: 10
+            //             }
+            //         }]
+            //     }
+            // };
+            // //
+            // var ctx = document.getElementById('chart1').getContext('2d');
+            // var chart = new Chart(ctx, {
+            //     type: "line",
+            //     data: {
+            //         labels: [1, 2, 1, 3, 5, 4, 7],
+            //         datasets: [{
+            //             backgroundColor: "rgba(101, 116, 205, 0.1)",
+            //             borderColor: "rgba(101, 116, 205, 0.8)",
+            //             borderWidth: 2,
+            //             data: [1, 2, 1, 3, 5, 4, 7],
+            //         }, ],
+            //     },
+            //     options: chartOptions
+            // });
+
+            // var ctx = document.getElementById('chart2').getContext('2d');
+            // var chart = new Chart(ctx, {
+            //     type: "line",
+            //     data: {
+            //         labels: [2, 3, 2, 9, 7, 7, 4],
+            //         datasets: [{
+            //             backgroundColor: "rgba(246, 109, 155, 0.1)",
+            //             borderColor: "rgba(246, 109, 155, 0.8)",
+            //             borderWidth: 2,
+            //             data: [2, 3, 2, 9, 7, 7, 4],
+            //         }, ],
+            //     },
+            //     options: chartOptions
+            // });
+            // //
+            // var ctx = document.getElementById('chart3').getContext('2d');
+            // var chart = new Chart(ctx, {
+            //     type: "line",
+            //     data: {
+            //         labels: [2, 5, 1, 3, 2, 6, 7],
+            //         datasets: [{
+            //             backgroundColor: "rgba(246, 153, 63, 0.1)",
+            //             borderColor: "rgba(246, 153, 63, 0.8)",
+            //             borderWidth: 2,
+            //             data: [2, 5, 1, 3, 2, 6, 7],
+            //         }, ],
+            //     },
+            //     options: chartOptions
+            // });
+
+
+            var data = {
+                labels: ["Payed", "Not Payed", "Canceled"],
+                datasets: [{
+                    data: [50, 25, 25],
+                    backgroundColor: ["#36A2EB", "#FFCE56", "#FF6384"]
+                }]
+            };
+
+            var options = {
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Booking Statistics'
+                    }
+                }
+            };
+
+            // Get the context of the canvas element
+            var ctx = document.getElementById("bookingChart").getContext("2d");
+            // Create the pie chart
+            var bookingChart = new Chart(ctx, {
+                type: 'pie',
+                data: data,
+                options: options
+            });
+
+            var data = {
+                labels: ['Site web', 'Agence'],
+                datasets: [{
+                    data: [25, 75]
+                }]
+            }
+
+            var options = {
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'booking types'
+                    }
+                }
+            }
+
+            var ctx = document.getElementById('bookingType').getContext("2d");
+            var bookingTypeChart = new Chart(ctx, {
+                type: 'pie',
+                data: data,
+                options: options
+            });
+
+
+        }
+    </script>
+
+
 </x-admin-layout>
