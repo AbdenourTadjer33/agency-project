afe156167ce87ab1c2d862d494fccc968bd41930
AbdenourTadjer33 <tad.abdenour33@gmail.com>
2023-12-17 02:23:44 +0100

    fix & update trip model

--- app/Models/Trip.php
+++ app/Models/Trip.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Trip extends Model
 {
@@ -11,11 +13,27 @@ class Trip extends Model
 
     protected $fillable = [
         'name',
+        'slug',
         'description',
         'destination',
-        'price_adult',
-        'price_chile',
-        'price_baby',
-        'reduction'
+        'category',
+        'formule_base',
+        'assets',
+        'hotel_id',
     ];
+
+    public $cast = [
+        'assets' => 'array',
+    ];
+
+
+    public function tripDates() : HasMany
+    {
+        return $this->hasMany(Trip::class);
+    }
+
+    public function pricing(): MorphOne
+    {
+        return $this->morphOne(Pricing::class, 'pricingable');
+    }
 }
