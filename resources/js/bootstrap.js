a49272f6bcd3f553beeaff6b437cf9259c022240
AbdenourTadjer33 <tad.abdenour33@gmail.com>
2023-12-18 21:39:55 +0100

    fix language for date picker

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -14,14 +14,42 @@ window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 import "flowbite";
 import Datepicker from "flowbite-datepicker/Datepicker";
+import DateRangePicker from "flowbite-datepicker/DateRangePicker";
 import fr from "flowbite-datepicker/locales/fr";
 
-const datepickerEl = document.querySelectorAll("[datepicker]").forEach(function (datepickerEl) {
+document.querySelectorAll("[datepicker]").forEach((datepickerEl) => {
     Object.assign(Datepicker.locales, fr);
     const datePicker = new Datepicker(datepickerEl, {
-      language: 'fr',
+        language: "fr",
+        format: "yyyy-mm-dd",
+        datepickerAutohide: true,
     });
+});
 
+document.querySelectorAll("[date-rangepicker]").forEach((datePickerRangeEL) => {
+    const dateRangePicker = new DateRangePicker(datePickerRangeEL, {
+        language: "fr",
+        format: "yyyy-mm-dd",
+        datepickerAutohide: true,
+    });
+});
+
+document.addEventListener("new-datepicker", (event) => {
+    Object.assign(Datepicker.locales, fr);
+    const datePicker = new Datepicker(event.detail.element, {
+        language: "fr",
+        format: "yyyy-mm-dd",
+        datepickerAutohide: true,
+    });
+});
+
+document.addEventListener("new-daterangepicker", (event) => {
+  Object.assign(Datepicker.locales, fr);
+  const datePicker = new DateRangePicker(event.detail.element, {
+      language: "fr",
+      format: "yyyy-mm-dd",
+      datepickerAutohide: true,
+  });
 });
 
 import Chart from "chart.js/auto";
